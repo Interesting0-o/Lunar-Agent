fc056fb5fcc7dd8f2339c363fefd08d500df115a
Interesing0 <16584423+interesing0@user.noreply.gitee.com>
2026-06-09 11:36:50 +0800

    init:初始化web服务

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from luna!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def home():
+    return {"message": "Hello World"}
